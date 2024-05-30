bd9c224cfa6f65f5a8bd47e718197325951bdb55
Amber <aqscode@gmail.com>
2024-05-30 16:25:55 -0600

    Rename to allow for modularity

---
+++ ShapeArea/CalculatorParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeArea
+{
+  public struct CalculatorParameters
+  {
+    public float PI; // Can be used to specify a decimal limit for PI.
+    public int decimalPlaces; // Used to determine where rounding occurs
+    public ERounding rounding; // Used to determine rounding behavior
+  }
+}
---
+++ ShapeArea/ShapeCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace ShapeArea
+{
+  public static class ShapeCalculator
+  {
+    private static CalculatorParameters areaParameters;
+    public static void SetupDefaultParameters(CalculatorParameters param)
+    {
+      areaParameters = param;
+    }
+
+
+  }
+}
